aaf343448650f126e90cc1e6a7244bcc4774a6c2
mingcw <mingc2016@126.com>
2018-05-01 01:16:55 +0800

    定制403页面

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>禁止访问 403</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body class="app sidebar-mini rtl">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>&nbsp;</p>
+          <div class="bs-component">
+            <div class="alert alert-dismissible alert-danger">
+              <h1 class="tile-title text-center">禁止访问 403</h1>
+              <p>对不起，您没有访问权限该页面。造成该情况的原因主要如下：</p>
+              <ul>
+                <li>您的IP被列入黑名单</li>
+                <li>您发送的请求过多</li>
+                <li>服务器繁忙</li>
+                <li>网站空间未绑定域名</li>
+                <li>您对该文件/目录不具有读/执行权限</li>
+              </ul>
+              <p class="tile-footer"><a class="btn btn-info" href="javascript:history.go(-1);"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i>回到上一页</a></p>
+            </div>
+          </div>
+            
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+    
+
+    
