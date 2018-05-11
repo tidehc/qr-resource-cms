f90f57cc79c52d056a6eebfbe0ef192b251be9a0
mingcw <mingc2016@126.com>
2018-05-11 14:12:25 +0800

    优化前端提示UI

--- resources/views/admin/errors.blade.php
+++ resources/views/admin/errors.blade.php
@@ -1,6 +1,5 @@
 @if (count($errors) > 0)
 <div class="alert alert-danger">
-  <button class="close" type="button" data-dismiss="alert">×</button>
   <ul>
     @foreach ($errors->all() as $error)
       <li>{{ $error }}</li>
--- resources/views/index/success.blade.php
+++ resources/views/index/success.blade.php
@@ -1,6 +1,5 @@
 @if (session('success'))
   <div class="alert alert-success">
-    <button class="close" type="button" data-dismiss="alert">×</button>
     {{ session('success') }}
   </div>
 @endif
